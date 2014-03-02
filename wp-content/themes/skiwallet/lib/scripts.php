8f28adba9c637bbb482a1e67933c68309a93e041
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-02 16:47:01 +0100

    Change global style and front page layout

--- wp-content/themes/skiwallet/lib/scripts.php
+++ wp-content/themes/skiwallet/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '55351705fa5d4a4b6617a7c155bdf258');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9e27cf5449be55d60e3c1572aba2661c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
