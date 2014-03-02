f56f8804fbe47eca3d74f1d3813482a671dc6ba8
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-02 17:41:04 +0100

    Minor style change for featuerd images

--- wp-content/themes/skiwallet/lib/scripts.php
+++ wp-content/themes/skiwallet/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd6cf26368f8dd85e4ec65085d78a5865');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'be8e15da378d179eed24fcd85dded13c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
