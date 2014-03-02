b0054b97c02c9655a89cd89deb6bd2976a0f6b42
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-02 17:34:13 +0100

    Change post meta order

--- wp-content/themes/skiwallet/templates/entry-meta.php
+++ wp-content/themes/skiwallet/templates/entry-meta.php
@@ -1,9 +1,10 @@
-<time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 <p class="byline author vcard">
   <?php echo __('Written by', 'roots'); ?>
   <a href="<?php echo get_the_author_meta('user_url'); ?>?rel=author" rel="author" class="fn">
     <?php echo get_the_author(); ?>
   </a>
+  <?php echo __('on', 'roots'); ?>
+  <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 </p>
 
 <? if (has_post_thumbnail()) { // Featured image or featured video
