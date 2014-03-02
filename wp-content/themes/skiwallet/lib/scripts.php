f5ae62aec82663f00ed5991ebe4720b2e55e155c
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-02 18:27:54 +0100

    Remove post titles top margin

--- wp-content/themes/skiwallet/lib/scripts.php
+++ wp-content/themes/skiwallet/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0fdef4eab6e909b2668e31cc4232d6c8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2d212fa96733aac95619be7bb9919f7f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
