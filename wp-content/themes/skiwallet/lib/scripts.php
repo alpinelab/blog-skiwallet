5873cfdc1ef3b33a615bee43dd3883a31e054d6b
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-02 17:52:44 +0100

    Change alerts colors

--- wp-content/themes/skiwallet/lib/scripts.php
+++ wp-content/themes/skiwallet/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'be8e15da378d179eed24fcd85dded13c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0c97f51ec7fe0bda59c07dfe36898947');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
