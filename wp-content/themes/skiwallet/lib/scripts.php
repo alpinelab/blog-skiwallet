fe20003c874979dd9050bf70b877ff164d6a8ea7
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-02 19:08:28 +0100

    Add style for CallToAction widget

--- wp-content/themes/skiwallet/lib/scripts.php
+++ wp-content/themes/skiwallet/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2d212fa96733aac95619be7bb9919f7f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a0f0701f820aa16adee762f9b79b8a6f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
