856bc2979f8ce9b56d420236e52e42cad9974f06
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-02 17:57:42 +0100

    Add h1 and image alt in header

--- wp-content/themes/skiwallet/lib/scripts.php
+++ wp-content/themes/skiwallet/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0c97f51ec7fe0bda59c07dfe36898947');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0fdef4eab6e909b2668e31cc4232d6c8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
