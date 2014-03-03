3752c615db78530a89cfd5b48ca062308db058f1
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-03 13:57:53 +0100

    Change title colors

--- wp-content/themes/skiwallet/lib/scripts.php
+++ wp-content/themes/skiwallet/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b8201658169fc7d419382ac4b3666013');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2b457d1a814b644c4ece67db3fd6f1db');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
