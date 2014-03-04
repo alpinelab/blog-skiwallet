1b45a7f11a5c23904bc5233fd5e53bf68b3c9ad8
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-04 02:18:29 +0100

    Add [thumbs_rating] shortcode

--- wp-content/themes/skiwallet/lib/custom.php
+++ wp-content/themes/skiwallet/lib/custom.php
@@ -7,3 +7,9 @@
 function image_asset($filename) {
   return get_stylesheet_directory_uri() . '/assets/img/' . $filename;
 }
+
+// Shortcode: [thumbs_rating]
+function thumbs_rating_func() {
+  return function_exists('thumbs_rating_getlink') ? thumbs_rating_getlink() : '';
+}
+add_shortcode('thumbs_rating', 'thumbs_rating_func' );
