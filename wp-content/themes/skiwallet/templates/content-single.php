474f97c68a6fc4617c00e653c782945c9ffc14cc
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-04 02:54:48 +0100

    Add PHP snippet to display WP SEO breadcrumbs on single posts

--- wp-content/themes/skiwallet/templates/content-single.php
+++ wp-content/themes/skiwallet/templates/content-single.php
@@ -1,6 +1,9 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
+      <?php if (function_exists('yoast_breadcrumb')) {
+        yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+      } ?>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
