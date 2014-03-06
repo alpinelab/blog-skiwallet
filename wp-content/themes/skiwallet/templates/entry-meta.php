e70f221cce9f64cbbf6f5379c74b3145848fb246
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-06 04:39:53 +0100

    Add post category in entry-title line

--- wp-content/themes/skiwallet/templates/entry-meta.php
+++ wp-content/themes/skiwallet/templates/entry-meta.php
@@ -1,10 +1,16 @@
 <p class="byline author vcard">
-  <?php echo __('Written by', 'roots'); ?>
-  <a href="<?php echo get_the_author_meta('user_url'); ?>?rel=author" rel="author" class="fn">
-    <?php echo get_the_author(); ?>
+
+  <?= __('Written by', 'roots') ?>
+  <a href="<?= get_the_author_meta('user_url') ?>?rel=author" rel="author" class="fn">
+    <?= get_the_author() ?>
   </a>
-  <?php echo __('on', 'roots'); ?>
-  <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+
+  <?= __('in', 'roots') ?>
+  <? the_category(', ') ?>
+
+  <?= __('on', 'roots') ?>
+  <time class="published" datetime="<?= get_the_time('c') ?>"><?= get_the_date() ?></time>
+
 </p>
 
 <? if (has_post_thumbnail()) { // Featured image or featured video
