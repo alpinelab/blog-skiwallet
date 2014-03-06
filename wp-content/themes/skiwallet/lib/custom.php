14c1ad747ae54faab7879f68e3cdd32cfc2f40b5
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-06 21:50:38 +0100

    Customize ReadMore link

--- wp-content/themes/skiwallet/lib/custom.php
+++ wp-content/themes/skiwallet/lib/custom.php
@@ -13,3 +13,9 @@ function thumbs_rating_func() {
   return function_exists('thumbs_rating_getlink') ? thumbs_rating_getlink() : '';
 }
 add_shortcode('thumbs_rating', 'thumbs_rating_func' );
+
+// Override ReadMore link
+function readmore_link() {
+  return '<a class="more-link btn btn-primary btn-xs" href="' . get_permalink() . '" rel="nofollow"><i class="fa fa-plus"></i> ' . __('More...') . '</a>';
+}
+add_filter('the_content_more_link', 'readmore_link');
